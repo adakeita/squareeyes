f7433c848bdb0d359444bad9f88db437fb0c2335
Ada Keita <mathildebrakstad@gmail.com>
2023-03-09 14:11:59 +0100

    headerInteraction

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
+//NAVBAR
+window.addEventListener("load", () => {
+    const navbar = document.getElementById("navbar");
+    const hamburger = navbar.querySelector(".hamburger");
+    const menu = navbar.querySelector(".menu");
+  
+    hamburger.addEventListener("click", () => {
+      navbar.classList.toggle("showMenu");
+    });
+  
+    menu.addEventListener("click", (event) => {
+      if (event.target.tagName === "A") {
+        navbar.classList.remove("showMenu");
+      }
+    });
+  
+    window.addEventListener("scroll", () => {
+      const logo = navbar.querySelector(".logoimage");
+      if (document.documentElement.scrollTop > 65 || document.body.scrollTop > 65) {
+        navbar.classList.add("scrolled");
+        logo.classList.add("small");
+        hamburger.classList.remove("hide");
+        menu.classList.add("hide");
+      } else {
+        navbar.classList.remove("scrolled");
+        logo.classList.remove("small");
+        hamburger.classList.add("hide");
+        menu.classList.remove("hide");
+      }
+    });
+  });
+
+
+
+//FAQ
 const faq = document.querySelectorAll(".faq-question");
 
 faq.forEach((item) => {
