d23dace7d525dffff38b549f5c6f538416788f4f
Ada Keita <mathildebrakstad@gmail.com>
2023-03-09 21:18:22 +0100

    headerInteraction

--- script.js
+++ script.js
@@ -1,34 +1,41 @@
-//NAVBAR
 window.addEventListener("load", () => {
     const navbar = document.getElementById("navbar");
     const hamburger = navbar.querySelector(".hamburger");
     const menu = navbar.querySelector(".menu");
-  
-    hamburger.addEventListener("click", () => {
-      navbar.classList.toggle("showMenu");
-    });
-  
-    menu.addEventListener("click", (event) => {
-      if (event.target.tagName === "A") {
-        navbar.classList.remove("showMenu");
-      }
-    });
-  
+    const navItem = navbar.querySelectorAll(".item_show");
+    const menuIcon = hamburger.querySelector(".menuIcon");
+    const closeIcon = hamburger.querySelector(".closeIcon");
+
+    menu.classList.add("closed");
+    closeIcon.classList.add("hide");
+
     window.addEventListener("scroll", () => {
-      const logo = navbar.querySelector(".logoimage");
-      if (document.documentElement.scrollTop > 65 || document.body.scrollTop > 65) {
-        navbar.classList.add("scrolled");
-        logo.classList.add("small");
-        hamburger.classList.remove("hide");
-        menu.classList.add("hide");
-      } else {
-        navbar.classList.remove("scrolled");
-        logo.classList.remove("small");
-        hamburger.classList.add("hide");
-        menu.classList.remove("hide");
-      }
+        const logo = navbar.querySelector(".logoimage");
+        const scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+        const isScrolled = scrollTop > 60;
+        navbar.classList.toggle("scrolled", isScrolled);
+        logo.classList.toggle("small", isScrolled);
+        hamburger.classList.toggle("hide", !isScrolled);
+        menu.classList.toggle("hide", isScrolled);
     });
-  });
+
+    hamburger.addEventListener("click", () => {
+        if (menu.classList.contains("closed")) {
+            menu.classList.remove("closed");
+            menu.classList.add("open");
+            menuIcon.classList.add("hide");
+            closeIcon.classList.remove("hide");
+        } else {
+            menu.classList.remove("open");
+            menu.classList.add("closed");
+            closeIcon.classList.add("hide");
+            menuIcon.classList.remove("hide");
+        }
+        navItem.forEach(item => {
+            item.classList.toggle("show");
+        });
+    });
+});
 
 
 
@@ -36,20 +43,20 @@ window.addEventListener("load", () => {
 const faq = document.querySelectorAll(".faq-question");
 
 faq.forEach((item) => {
-  const toggle = item.querySelector(".faq-toggle");
+    const toggle = item.querySelector(".faq-toggle");
 
-  item.addEventListener("click", () => {
-    // Toggle between adding and removing the "active" class,
-    // to highlight the button that controls the panel
-    item.classList.toggle("active");
+    item.addEventListener("click", () => {
+        // Toggle between adding and removing the "active" class,
+        // to highlight the button that controls the panel
+        item.classList.toggle("active");
 
-    // Toggle between hiding and showing the active panel
-    const answer = item.nextElementSibling;
-    answer.style.display = answer.style.display === "block" ? "none" : "block";
+        // Toggle between hiding and showing the active panel
+        const answer = item.nextElementSibling;
+        answer.style.display = answer.style.display === "block" ? "none" : "block";
 
-    // Toggle the plus/minus sign
-    toggle.textContent = toggle.textContent === "+" ? "-" : "+";
-  });
+        // Toggle the plus/minus sign
+        toggle.textContent = toggle.textContent === "+" ? "-" : "+";
+    });
 });
 
 
